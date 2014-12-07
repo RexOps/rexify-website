5bfe64231764af353201eab5e43757f5b8f53b2a
Jan <krimdomu@googlemail.com>
2014-12-07 19:01:09 +0100

    directories

--- html/run.sh
+++ html/run.sh
@@ -3,7 +3,11 @@
 cd /var/tmp
 git clone https://github.com/RexOps/Rex.git rex
 cd rex
-mkdir -p doc/html/Rex/{Box,Commands,Virtualization,FS}
+mkdir -p doc/html/Rex/Box
+mkdir -p doc/html/Rex/Commands
+mkdir -p doc/html/Rex/Virtualization
+mkdir -p doc/html/Rex/FS
+
 misc/create_pod.sh
 
 cp -R doc/html/Rex* /var/project/templates/api/
