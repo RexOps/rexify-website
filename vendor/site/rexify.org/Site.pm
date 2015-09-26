88262dfd185f9ff254c0e8baaeec552fc6b28d2e
Jan <krimdomu@googlemail.com>
2015-09-26 23:41:55 +0200

    docs, api and release notes

--- vendor/site/rexify.org/Site.pm
+++ vendor/site/rexify.org/Site.pm
@@ -1,6 +1,7 @@
 package Site;
 
 use Mojo::Base 'Pitahaya::PageType';
+use version;
 
 sub GET {
   my ($self) = @_;
@@ -25,17 +26,31 @@ sub page_not_found {
   my $url = $self->controller->req->url;
 
   my %redir_map = (
-    "/get" => "/get.html",
     "/howtos/perl.html" => "/docs/guides/just_enough_perl_for_rex.html",
     "/contribute" => "/care/help__r__ex.html",
     "/contribute/donate.html" => "/care/help_people_in_need.html",
     "/howtos/index.html" => "/docs.html",
     "/howtos/faq.html" => "/docs/faq.html",
+    "/howtos/start.html" => "/docs/guides/start_using__r__ex.html",
   );
 
   if(exists $redir_map{$url}) {
     $self->controller->res->code(301);
     $self->controller->redirect_to($redir_map{$url});
+  }
+  elsif($url =~ m/^\/api\/(.*)/) {
+    # api page call
+    my $api_part = $1;
+    my $api_root = $self->site->get_page(29);
+    my @children = $api_root->children;
+    my ($newest) = sort { version->parse("v" . $b->name) <=> version->parse("v" . $a->name) } @children;
+    
+    my $api_url = $newest->generate_url;
+    $api_url =~ s/\.html$//;
+    $api_url .= "/$api_part";
+    
+    $self->controller->res->code(302);
+    $self->controller->redirect_to(lc($api_url));
   }
   else {
     $self->controller->reply->not_found();
