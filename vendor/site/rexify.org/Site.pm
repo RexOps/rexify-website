f312a0ea7b0060021ffba9203ccc130ca68ee06c
Jan <krimdomu@googlemail.com>
2015-09-26 13:21:58 +0200

    added page not found handler

--- vendor/site/rexify.org/Site.pm
+++ vendor/site/rexify.org/Site.pm
@@ -19,4 +19,27 @@ sub GET {
 
 }
 
+sub page_not_found {
+  my ($self) = @_;
+
+  my $url = $self->controller->req->url;
+
+  my %redir_map = (
+    "/get" => "/get.html",
+    "/howtos/perl.html" => "/docs/guides/just_enough_perl_for_rex.html",
+    "/contribute" => "/care/help__r__ex.html",
+    "/contribute/donate.html" => "/care/help_people_in_need.html",
+    "/howtos/index.html" => "/docs.html",
+    "/howtos/faq.html" => "/docs/faq.html",
+  );
+
+  if(exists $redir_map{$url}) {
+    $self->controller->res->code(301);
+    $self->controller->redirect_to($redir_map{$url});
+  }
+  else {
+    $self->controller->reply->not_found();
+  }
+}
+
 1;
