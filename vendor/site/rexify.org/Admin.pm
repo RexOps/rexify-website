3301f61aaddef3e4a1dd84cafa6d94adda875d2c
Jan <krimdomu@googlemail.com>
2015-10-02 19:09:18 +0200

    added cloudfront

---
+++ vendor/site/rexify.org/Admin.pm
@@ -0,0 +1,18 @@
+package Admin;
+
+use Mojo::Base 'Pitahaya::PageType';
+use version;
+
+sub page_POST {
+  my ($self) = @_;
+}
+
+sub page_PUT {
+  my ($self) = @_;
+}
+
+sub desktop {
+  my ($self) = @_;
+}
+
+1;
